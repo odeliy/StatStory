1d4ee80c337aae642f2341af078633dca12c019b
cody iott <codyleeiott@gmail.com>
2022-04-26 13:30:14 -0700

    first draft

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ hamburger.addEventListener("click", () => {
 })
 
 /**********************************/
-/*** Carousels ********************/
+/*** Center Scroll ****************/
+/**********************************/
+/* 1. center horizontal scroll section */
+document.getElementById("scrollCenter").scrollIntoView({
+    inline: "center"
+})
+
+/* 2. reset browser to top of page */
+document.getElementById("header").scrollIntoView({
+    block: "start"
+})
+
+/**********************************/
+/*** Carousel *********************/
 /**********************************/
 let position = 0
 const slides = document.getElementsByClassName("cstud__item")
@@ -46,3 +59,41 @@ document.getElementById("case-studies-next").addEventListener("click", () => {
 
     slides[position].classList.add("visible")
 })
+
+/**********************************/
+/*** Trusted By Animation *********/
+/**********************************/
+const icon = document.getElementById("main-icon")
+const iconArray = [
+    "./resources/images/pat-lotz.png",
+    "./resources/images/fishing-podcast.png",
+    "./resources/images/fishing-podcast.png"
+]
+let iconArrayPos = 0
+
+setInterval(changeIcon, 3000)
+
+function changeIcon() {
+    if(window.screen.width <= 650) {
+        iconArrayPos++
+
+        if(iconArrayPos === iconArray.length) {
+            iconArrayPos = 0
+        }
+    
+        /* remove animation class, so it can be retriggered */
+        icon.classList.remove("animate")
+        /* reflow dom element */
+        icon.offsetWidth
+        /* add animation class again */
+        icon.classList.add("animate")
+    
+        icon.src = iconArray[iconArrayPos]
+    }
+
+    /* if screen is resized, set to default at next interval */
+    if(window.screen.width >= 651) {
+        iconArrayPos = 0
+        icon.src = iconArray[iconArrayPos]
+    }
+}

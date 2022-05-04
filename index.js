fa4f0c942ef9eb3b8df7283f2a3c7959046398a6
cody iott <codyleeiott@gmail.com>
2022-05-04 14:40:06 -0700

    formatting with prettier

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 /**********************************/
 /*** navigation *******************/
 /**********************************/
-const hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", () => {
-    hamburger.nextElementSibling.classList.toggle("mobile-hidden")
-})
+  hamburger.nextElementSibling.classList.toggle("mobile-hidden");
+});
 
 /**********************************/
 /*** infinity scroll **************/
 /**********************************/
-const scrollBanner = document.getElementById("scroll-banner")
+const scrollBanner = document.getElementById("scroll-banner");
 
 scrollBanner.addEventListener("mouseenter", () => {
-    scrollBanner.style.animationPlayState = "paused"
-})
+  scrollBanner.style.animationPlayState = "paused";
+});
 
 scrollBanner.addEventListener("mouseleave", () => {
-    scrollBanner.style.animationPlayState = "running"
-})
+  scrollBanner.style.animationPlayState = "running";
+});
 
 /**********************************/
 /*** carousel *********************/
 /**********************************/
-let position = 0
-const slides = document.getElementsByClassName("cstud__item")
-const numOfSlides = slides.length
-
+let position = 0;
+const slides = document.getElementsByClassName("cstud__item");
+const numOfSlides = slides.length;
 
 function hideContent(content) {
-    for (let item of content) {
-        item.classList.remove("visible")
-    }
+  for (let item of content) {
+    item.classList.remove("visible");
+  }
 }
 
 /* Previous Arrow */
 document.getElementById("case-studies-prev").addEventListener("click", () => {
-    hideContent(slides)
+  hideContent(slides);
 
-    if(position === 0) {
-        position = numOfSlides - 1
-    } else {
-        position--
-    }
+  if (position === 0) {
+    position = numOfSlides - 1;
+  } else {
+    position--;
+  }
 
-    slides[position].classList.add("visible")
-})
+  slides[position].classList.add("visible");
+});
 
 /* Next Arrow */
 document.getElementById("case-studies-next").addEventListener("click", () => {
-    hideContent(slides)
+  hideContent(slides);
 
-    if(position === numOfSlides - 1) {
-        position = 0
-    } else {
-        position++
-    }
+  if (position === numOfSlides - 1) {
+    position = 0;
+  } else {
+    position++;
+  }
 
-    slides[position].classList.add("visible")
-})
+  slides[position].classList.add("visible");
+});

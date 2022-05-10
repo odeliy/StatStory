75a81dd16d02e11865c1f7eddf198992e7510e06
cody iott <codyleeiott@gmail.com>
2022-05-10 10:17:23 -0700

    carousel bug squash

--- index.js
+++ index.js
@@ -21,45 +21,6 @@ scrollBanner.addEventListener("mouseleave", () => {
 });
 
 /**********************************/
-/*** carousel *********************/
-/**********************************/
-let position = 0;
-const slides = document.getElementsByClassName("cstud__item");
-const numOfSlides = slides.length;
-
-function hideContent(content) {
-  for (let item of content) {
-    item.classList.remove("visible");
-  }
-}
-
-/* Previous Arrow */
-document.getElementById("case-studies-prev").addEventListener("click", () => {
-  hideContent(slides);
-
-  if (position === 0) {
-    position = numOfSlides - 1;
-  } else {
-    position--;
-  }
-
-  slides[position].classList.add("visible");
-});
-
-/* Next Arrow */
-document.getElementById("case-studies-next").addEventListener("click", () => {
-  hideContent(slides);
-
-  if (position === numOfSlides - 1) {
-    position = 0;
-  } else {
-    position++;
-  }
-
-  slides[position].classList.add("visible");
-});
-
-/**********************************/
 /*** Fade In Bullets **************/
 /**********************************/
 function addClass(entries) {
@@ -102,3 +63,113 @@ const backgroundImg2Observer = new IntersectionObserver((entries) => {
   }
 });
 backgroundImg2Observer.observe(document.querySelector(".success__ghost"));
+
+/**********************************/
+/*** carousel *********************/
+/**********************************/
+// let position = 0;
+// const slides = document.getElementsByClassName("cstud__item");
+// const numOfSlides = slides.length;
+
+// function hideContent(content) {
+//   for (let item of content) {
+//     item.classList.remove("visible");
+//   }
+// }
+
+// document.getElementById("case-studies-prev").addEventListener("click", () => {
+//   hideContent(slides);
+
+//   if (position === 0) {
+//     position = numOfSlides - 1;
+//   } else {
+//     position--;
+//   }
+
+//   slides[position].classList.add("visible");
+// });
+
+// document.getElementById("case-studies-next").addEventListener("click", () => {
+//   hideContent(slides);
+
+//   if (position === numOfSlides - 1) {
+//     position = 0;
+//   } else {
+//     position++;
+//   }
+
+//   slides[position].classList.add("visible");
+// });
+let position = 0;
+const slides = [
+  {
+    id: 1,
+    href: "./case-studies/case-studies.html",
+    person: "Penny Kervill",
+    occupation: "Realtor",
+    subsUp: "304%",
+    viewsUp: "451%",
+    leadsUp: "3X",
+  },
+
+  {
+    id: 2,
+    href: "./case-studies/case-studies.html",
+    person: "Jane Smith",
+    occupation: "Occupation",
+    subsUp: "300%",
+    viewsUp: "260%",
+    leadsUp: "2X",
+  },
+
+  {
+    id: 3,
+    href: "./case-studies/case-studies.html",
+    person: "Joe Smith",
+    occupation: "Spa Repair",
+    subsUp: "980%",
+    viewsUp: "40%",
+    leadsUp: "9X",
+  },
+];
+const numOfSlides = slides.length;
+
+const carousel = document.getElementById("carousel");
+
+function renderHtml() {
+  carousel.innerHTML = `
+    <a
+      class="cstud__item"
+      href=${slides[position].href}
+    >
+      <span class="cstud__person">${slides[position].person} | ${slides[position].occupation}</span>
+      <span class="cstud__subscribers">${slides[position].subsUp}</span>
+      <span class="cstud__views">${slides[position].viewsUp}</span>
+      <span class="cstud__leads">${slides[position].leadsUp}</span>
+    </a>
+`;
+}
+
+renderHtml();
+
+const prevArrow = document.getElementById("case-studies-prev");
+prevArrow.addEventListener("click", () => {
+  if (position === 0) {
+    position = numOfSlides - 1;
+  } else {
+    position--;
+  }
+
+  renderHtml();
+});
+
+const nextArrow = document.getElementById("case-studies-next");
+nextArrow.addEventListener("click", () => {
+  if (position === numOfSlides - 1) {
+    position = 0;
+  } else {
+    position++;
+  }
+
+  renderHtml();
+});
